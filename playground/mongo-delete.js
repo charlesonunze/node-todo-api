75d5877f5984b0d9a68ec397b31ee2074e8c05c6
charlesonunze <charlesonunze2@gmail.com>
2018-03-26 22:11:02 +0100

    Add delete script

--- playground/mongo-delete.js
+++ playground/mongo-delete.js
@@ -11,7 +11,8 @@ MongoClient.connect( 'mongodb://localhost:27017', ( err, client ) => {
   console.log( 'Connected to MongoDB server' );
 
   const db = client.db( dbName );
-  //delete many
+
+  // delete many
   db
     .collection( 'Todos' )
     .deleteMany( { text: 'Something to do' } )
@@ -19,9 +20,26 @@ MongoClient.connect( 'mongodb://localhost:27017', ( err, client ) => {
       console.log( result );
     }, ( err ) => {
       console.log( err );
-    } )
-  //delete one
-  // findone and delete
+    } );
 
+  // delete one
+  db
+    .collection( 'Todos' )
+    .deleteOne( { text: 'Something to do' } )
+    .then( ( result ) => {
+      console.log( result );
+    }, ( err ) => {
+      console.log( err );
+    } );
+
+  // findone and delete
+  db
+    .collection( 'Todos' )
+    .findOneAndDelete( { text: 'Something to do' } )
+    .then( ( result ) => {
+      console.log( result );
+    }, ( err ) => {
+      console.log( err );
+    } );
   // client.close();
 } );
