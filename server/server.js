ceec49e932be60da9fbaae11c068d68fab1f478e
charlesonunze <charlesonunze2@gmail.com>
2018-03-29 18:28:37 +0100

    Added DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -54,6 +54,26 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) return res.status(404)
+    .send({ Error: 'Invalid ID' });
+
+  Todo
+    .findByIdAndDelete(id)
+    .then((todo) => {
+      if (!todo) return res.status(404)
+        .send({ Error: 'Page not found' });
+
+      res.send({ todo });
+    })
+    .catch((e) => {
+      res.status(400)
+        .send(e);
+    });
+});
+
 app.listen(3000, () => {
   console.log('Server started...');
 });
