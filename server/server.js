2ef8ee7ef3272bb83015477d7177f35688352fe3
charlesonunze <charlesonunze2@gmail.com>
2018-03-29 17:34:56 +0100

    Refactored error handling and added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,38 +1,59 @@
-const express = require( 'express' );
-const bodyParser = require( 'body-parser' );
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
-const { mongoose } = require( './db/mongoose' );
-const { User } = require( './models/user' );
-const { Todo } = require( './models/todo' );
+const { mongoose } = require('./db/mongoose');
+const { User } = require('./models/user');
+const { Todo } = require('./models/todo');
 
 const app = express();
 
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
 
-app.post( '/todos', ( req, res ) => {
-
-  const todo = new Todo( { text: req.body.text } );
+app.post('/todos', (req, res) => {
+  const todo = new Todo({ text: req.body.text });
 
   todo
     .save()
-    .then( ( doc ) => {
-      res.send( doc );
-    }, ( e ) => {
-      res.send( e );
-    } )
+    .then((doc) => {
+      res.send(doc);
+    }, (e) => {
+      res.status(400)
+        .send(e);
+    });
+});
 
-} );
-
-app.get( '/todos', ( req, res ) => {
+app.get('/todos', (req, res) => {
   Todo
     .find()
-    .then( ( todos ) => {
-      res.send( { todos } );
-    }, ( e ) => {
-      res.send( e );
-    } )
-} );
+    .then((todos) => {
+      res.send({ todos });
+    }, (e) => {
+      res.status(400)
+        .send(e);
+    });
+});
 
-app.listen( 3000, () => {
-  console.log( 'Server started...' );
-} );
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) return res.status(404)
+    .send({ Error: 'Invalid ID' });
+
+  Todo
+    .findById(id)
+    .then((todo) => {
+      if (!todo) return res.status(404)
+        .send({ Error: 'Page not found' });
+
+      res.send({ todo });
+    })
+    .catch((e) => {
+      res.status(400)
+        .send(e);
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Server started...');
+});
