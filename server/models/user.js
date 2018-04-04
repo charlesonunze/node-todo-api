8ed9a68ce3b194e8d949180b1d6ac16504860726
charlesonunze <charlesonunze2@gmail.com>
2018-04-04 20:05:40 +0100

    Added DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -71,6 +71,19 @@ UserSchema.methods.generateAuthToken = function() {
     });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  const user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {
+        token
+      }
+    }
+  });
+
+};
+
 UserSchema.statics.findByToken = function(token) {
   const User = this;
   const salt = 'hash-salt';
--- server/server.js
+++ server/server.js
@@ -130,7 +130,6 @@ app.patch('/todos/:id', (req, res) => {
 // USERS SECTION
 app.post('/users', (req, res) => {
   const body = _.pick(req.body, ['email', 'password']);
-
   const user = new User(body);
 
   user
@@ -150,12 +149,8 @@ app.post('/users', (req, res) => {
     });
 });
 
-app.get('/users/me', authenticate, (req, res) => {
-  res.send(req.user);
-});
-
 app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
+  const body = _.pick(req.body, ['email', 'password']);
 
   User
     .findByCredentials(body.email, body.password)
@@ -167,6 +162,26 @@ app.post('/users/login', (req, res) => {
             .header('x-auth', token)
             .send(user);
         });
+    })
+    .catch((e) => {
+      res
+        .status(400)
+        .send();
+    });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req
+    .user
+    .removeToken(req.token)
+    .then(() => {
+      res
+        .status(200)
+        .send();
     })
     .catch((e) => {
       res
